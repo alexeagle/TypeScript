93dbbe2a2d0b42cefd02ac949e4bc8ab6b5b5823
Alex Eagle <alexeagle@google.com>
2015-04-01 16:12:39 -0700

    remove IE11 ECMAScript Extensions section

--- bin/lib.d.ts
+++ bin/lib.d.ts
@@ -1802,48 +1802,8 @@ declare var DataView: {
     new (buffer: ArrayBuffer, byteOffset?: number, length?: number): DataView;
 }
 
-/////////////////////////////
-/// IE11 ECMAScript Extensions
-/////////////////////////////
+// alexeagle: removed section /// IE11 ECMAScript Extensions
 
-interface Map<K, V> {
-    clear(): void;
-    delete(key: K): boolean;
-    forEach(callbackfn: (value: V, index: K, map: Map<K, V>) => void, thisArg?: any): void;
-    get(key: K): V;
-    has(key: K): boolean;
-    set(key: K, value: V): Map<K, V>;
-    size: number;
-}
-declare var Map: {
-    new <K, V>(): Map<K, V>;
-    prototype: Map<any, any>;
-}
-
-interface WeakMap<K, V> {
-    clear(): void;
-    delete(key: K): boolean;
-    get(key: K): V;
-    has(key: K): boolean;
-    set(key: K, value: V): WeakMap<K, V>;
-}
-declare var WeakMap: {
-    new <K, V>(): WeakMap<K, V>;
-    prototype: WeakMap<any, any>;
-}
-
-interface Set<T> {
-    add(value: T): Set<T>;
-    clear(): void;
-    delete(value: T): boolean;
-    forEach(callbackfn: (value: T, index: T, set: Set<T>) => void, thisArg?: any): void;
-    has(value: T): boolean;
-    size: number;
-}
-declare var Set: {
-    new <T>(): Set<T>;
-    prototype: Set<any>;
-}
 /////////////////////////////
 /// ECMAScript Internationalization API 
 /////////////////////////////
