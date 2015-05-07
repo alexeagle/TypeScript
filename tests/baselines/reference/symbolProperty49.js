3bccda489e4d8e76ea62c21403f2284780fe1a56
Alex Eagle <alexeagle@google.com>
2015-05-06 17:29:16 -0700

    Revert "minify"
    This reverts commit cddc1867c44b6bed9095dc30f0b0f552cbc003a9.

---
+++ tests/baselines/reference/symbolProperty49.js
@@ -0,0 +1,17 @@
+//// [symbolProperty49.ts]
+module M {
+    export var Symbol;
+
+    class C {
+        [Symbol.iterator]() { }
+    }
+}
+
+//// [symbolProperty49.js]
+var M;
+(function (M) {
+    M.Symbol;
+    class C {
+        [M.Symbol.iterator]() { }
+    }
+})(M || (M = {}));
